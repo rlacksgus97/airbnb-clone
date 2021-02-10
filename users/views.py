c631bd5b563027414124df159d5c15d54d369227
rlacksgus97 <rlacksgus97@naver.com>
2021-02-11 00:53:07 +0900

    #21.4 User Profile part Two

--- users/views.py
+++ users/views.py
@@ -159,3 +159,8 @@ def kakao_callback(request):
 class UserProfileView(DetailView):
     model = models.User
     context_object_name = 'user_obj'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["hello"] = "Hello!"
+        return context
