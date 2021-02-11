f3c2e1a98152ae44afbc6536de1499098935f083
rlacksgus97 <rlacksgus97@naver.com>
2021-02-12 01:06:43 +0900

    #21.8 Customizing Stuff

--- users/views.py
+++ users/views.py
@@ -182,13 +182,18 @@ class UpdateProfileView(UpdateView):
     def get_object(self, queryset=None):
         return self.request.user
 
-    def form_valid(self, form):
-        email = form.cleaned_data.get("email")
-        self.object.username = email
-        self.object.save()
-        return super().form_valid(form)
+    def get_form(self, form_class=None):
+        form = super().get_form(form_class=form_class)
+        form.fields['birthdate'].widget.attrs = {'placeholder': "Birthdate"}
+        return form
 
 class UpdatePasswordView(PasswordChangeView):
 
     template_name = "users/update-password.html"
-    pass
+    
+    def get_form(self, form_class=None):
+        form = super().get_form(form_class=form_class)
+        form.fields['old_password'].widget.attrs = {'placeholder': "Current Password"}
+        form.fields['new_password1'].widget.attrs = {'placeholder': "New Password"}
+        form.fields['new_password2'].widget.attrs = {'placeholder': "Confirm New Password"}        
+        return form
