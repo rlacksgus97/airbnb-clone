41c08153a82076716ac13c15f630d290ba714e97
rlacksgus97 <rlacksgus97@naver.com>
2021-02-12 00:39:57 +0900

    #21.7 Change Password

--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 import os
 import requests
-from django.views import View
+from django.contrib.auth.views import PasswordChangeView
 from django.views.generic import FormView, DetailView, UpdateView
 from django.urls import reverse_lazy
 from django.shortcuts import render, redirect, reverse
@@ -169,9 +169,9 @@ class UpdateProfileView(UpdateView):
     model = models.User
     template_name = "users/update-profile.html"
     fields = (
+        "email",
         "first_name",
         "last_name",
-        "avatar",
         "gender",
         "bio",
         "birthdate",
@@ -181,3 +181,14 @@ class UpdateProfileView(UpdateView):
 
     def get_object(self, queryset=None):
         return self.request.user
+
+    def form_valid(self, form):
+        email = form.cleaned_data.get("email")
+        self.object.username = email
+        self.object.save()
+        return super().form_valid(form)
+
+class UpdatePasswordView(PasswordChangeView):
+
+    template_name = "users/update-password.html"
+    pass
