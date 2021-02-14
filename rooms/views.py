0b304b5964abf4f3338a4e2697df0f94524f0ea7
rlacksgus97 <rlacksgus97@naver.com>
2021-02-14 16:35:10 +0900

    #23.1 Room Photos part One

--- rooms/views.py
+++ rooms/views.py
@@ -1,6 +1,8 @@
+from django.http.response import Http404
 from django.views.generic import ListView, DetailView, View, UpdateView
 from django.shortcuts import render
 from django.core.paginator import Paginator
+from users import mixins as user_mixins
 from . import models, forms
 
 
@@ -91,7 +93,7 @@ class SearchView(View):
         
         return render(request, "rooms/search.html", {"form": form})
 
-class EditRoomView(UpdateView):
+class EditRoomView(user_mixins.LoggedInOnlyView, UpdateView):
 
     model = models.Room
     template_name = "rooms/room_edit.html"
@@ -114,3 +116,20 @@ class EditRoomView(UpdateView):
         "facilities",
         "house_rules",
     }
+
+    def get_object(self, queryset=None):
+        room = super().get_object(queryset=queryset)
+        if room.host.pk != self.request.user.pk:
+            raise Http404()
+        return room
+
+class RoomPhotosView(user_mixins.LoggedInOnlyView, DetailView):
+
+    model = models.Room
+    template_name = "room_photos.html"
+    
+    def get_object(self, queryset=None):
+        room = super().get_object(queryset=queryset)
+        if room.host.pk != self.request.user.pk:
+            raise Http404()
+        return room
